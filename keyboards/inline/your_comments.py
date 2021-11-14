ad5bb7f47c7e8f802a139d0b9460c11942a1a2c0
admin <baltabaeva005@mail.ru>
2021-11-15 01:56:56 +0600

    When user clicks on "my comments" button he can see his old comments

---
+++ keyboards/inline/your_comments.py
@@ -0,0 +1,6 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+your_comments_keybord = InlineKeyboardMarkup()
+inline_keyboard = {
+    InlineKeyboardButton(text="вот они)
+}
